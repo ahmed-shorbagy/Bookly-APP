6d471025ee9cb8943396a402b45d932ce55d3ede
ahmed-shorbagy <a7med.shorbagy@gmail.com>
2023-07-12 22:18:12 +0300

    laying out the splash view

--- lib/features/splash/peresentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/peresentation/views/widgets/splash_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
 class SplashViewBody extends StatelessWidget {
@@ -6,7 +7,19 @@ class SplashViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [],
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset(AssetsData.logo),
+        SizedBox(
+          height: 12,
+        ),
+        Text(
+          textAlign: TextAlign.center,
+          'Explore a Whole Free Amazing Books',
+          style: TextStyle(fontWeight: FontWeight.w500),
+        )
+      ],
     );
   }
 }
