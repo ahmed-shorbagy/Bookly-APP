aa301e1575ea0ecbd2f7c096bec0a6bad775b31b
ahmed-shorbagy <a7med.shorbagy@gmail.com>
2023-07-16 16:54:52 +0300

    adiing best seller list view and control scroll

--- lib/features/Home/peresentation/views/widgets/home_page_body.dart
+++ lib/features/Home/peresentation/views/widgets/home_page_body.dart
@@ -13,21 +13,47 @@ class HomePageBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Padding(
       padding: EdgeInsets.only(left: 30),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          CustomAppBar(),
-          FeaturedHorizontalList(),
-          SizedBox(
-            height: 51,
+      child: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CustomAppBar(),
+                FeaturedHorizontalList(),
+                SizedBox(
+                  height: 51,
+                ),
+                Text('Best Seller', style: Styles.textStyle18),
+                SizedBox(
+                  height: 20,
+                ),
+              ],
+            ),
           ),
-          Text('Best Seller', style: Styles.textStyle18),
-          SizedBox(
-            height: 20,
-          ),
-          BestSellerListITem()
+          SliverFillRemaining(
+            child: BestSellerListView(),
+          )
         ],
       ),
+    );
+  }
+}
+
+class BestSellerListView extends StatelessWidget {
+  const BestSellerListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      physics: NeverScrollableScrollPhysics(),
+      itemBuilder: (context, index) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10),
+          child: BestSellerListITem(),
+        );
+      },
+      itemCount: 6,
     );
   }
 }
